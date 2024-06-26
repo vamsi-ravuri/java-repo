8af5f46cef8ba1d946a471714d0b7c1cec1893d0
root <root@ip-172-31-84-200.ec2.internal>
2024-06-26 06:43:14 +0000

    commiting app.java from maven

---
+++ src/main/java/com/nichebit/App.java
@@ -0,0 +1,13 @@
+package com.nichebit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Vamsi Good Luck" );
+    }
+}
